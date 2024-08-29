1fdfac404a6361a481aa0a5b3fac1b5b0501494b
Alessandro Bertozzi <bertozzi@LAPTOP-UK0L1JD4>
2024-08-29 20:51:57 +0200

    add possibility to publish migrations

--- src/Providers/InspirationProvider.php
+++ src/Providers/InspirationProvider.php
@@ -16,5 +16,8 @@ class InspirationProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
         $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        $this->publishes([
+            __DIR__.'/../migrations' => database_path('migrations')
+        ], 'inspire-migrations');
     }
 }
